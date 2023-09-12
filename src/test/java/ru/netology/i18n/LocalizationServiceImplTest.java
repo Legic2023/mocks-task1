1521ab9093a34f9c61dee17c5b308e4d8b06cd40
Legic <Legic2007@yandex.ru>
2023-09-12 22:07:57 +0300

    first commit

--- src/test/java/ru/netology/i18n/LocalizationServiceImplTest.java
+++ src/test/java/ru/netology/i18n/LocalizationServiceImplTest.java
@@ -2,24 +2,23 @@ package ru.netology.i18n;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.entity.Country;
 
 public class LocalizationServiceImplTest {
 
     @ParameterizedTest
-    @EnumSource(Country.class)
-    public void testLocale_returnedText_check(Country country) {
+    @CsvSource({
+            "Добро пожаловать, RUSSIA",
+            "Welcome, USA"
+    })
+    public void testLocale_returnedText_check(String message, Country country) {
 //expected
-       String expected1 = "Добро пожаловать";
-       String expected2 = "Welcome";
+        String expected = message;
 //actual
         String actual = new LocalizationServiceImpl().locale(country);
 //assert
-        if (country == Country.RUSSIA) {
-            Assertions.assertEquals(expected1, actual);
-        } else {
-            Assertions.assertEquals(expected2, actual);
-        }
+        Assertions.assertEquals(expected, actual);
     }
 }
+
